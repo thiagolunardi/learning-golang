4acf9d43b3dc734471fe920a0aa83fde39e77382
Thiago Lunardi <thiago.lunardi@complevo.de>
2019-11-08 13:59:16 +0100

    More improvements

--- src/main.go
+++ src/main.go
@@ -1,38 +1,126 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"./models"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-// Item x
-type Item struct {
-	ID int
-	Done bool
-	Title string
-}
-
-// Items x
-type Items []Item
-
-var items Items
+var items = models.Items{}
 
 func main() {
-	http.HandleFunc("/", homeController)
-	http.ListenAndServe(":13000", nil)
+	httpPort := 13000
+
+	seedData()
+
+	log.Println("Server available at following address:")
+	log.Printf("    http://localhost:%d/", httpPort)
+
+  handler := requestHandler()
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", httpPort), logRequest(handler)))
+
+	log.Println("Terminated.")
 }
 
-func homeController(w http.ResponseWriter, r *http.Request) {
-	log.Println("request")
+func requestHandler () http.Handler {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", listItemsAction).Methods(http.MethodGet)
+	router.HandleFunc("/", addItemAction).Methods(http.MethodPost)
+	router.HandleFunc("/{id}", getItemAction).Methods(http.MethodGet)
+	router.HandleFunc("/{id}", updateItemAction).Methods(http.MethodPut)
+	router.HandleFunc("/{id}", deleteItemAction).Methods(http.MethodDelete)
+
+	return router
 }
 
 func listItemsAction(w http.ResponseWriter, r *http.Request) {
-	body, err = json.Marshal(items)
+	log.Println("listItemsAction")
+	respondJSON(w, items)
+}
+
+func addItemAction(w http.ResponseWriter, r *http.Request) {
+	log.Println("addItemAction")
+	var item models.Item
+	err := json.NewDecoder(r.Body).Decode(&item)
 	if err != nil {
-		log.Fatal("Error enconding Items")
+		log.Fatal("Cannot decode Item from JSON ", err)
 	}
-	fmt.Printf(body, w)
+	defer r.Body.Close()
+
+	item.ID = len(items) + 1
+	items = append(items, item)
+
+	respondJSON(w, item)
+}
+
+func updateItemAction(w http.ResponseWriter, r *http.Request) {
+	log.Println("updateItemAction")
+	id := getIDValue(r)
+	
+	item := findByID(id)
+
+	item.Done = true
+
+	respondJSON(w, item);
+
+}
+
+func deleteItemAction(w http.ResponseWriter, r *http.Request) {
+	log.Println("deleteItemAction")
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func getItemAction(w http.ResponseWriter, r *http.Request) {
+	log.Println("getItemAction")
+	id := getIDValue(r)
+
+	itemFound := findByID(int(id))
+
+	respondJSON(w, itemFound)
+}
+
+func getIDValue(r *http.Request) int {
+	id, err := strconv.ParseInt( mux.Vars(r)["id"], 0, 0 )
+	if err != nil {
+		log.Fatal("ID is not integer")		
+		return 0
+	}
+	return int(id)
+}
+
+func findByID(id int) *models.Item {
+  for _, item := range items {
+    if item.ID == id {
+      return &item
+    }
+  }
+  return nil
+}
+
+func respondJSON(w http.ResponseWriter, data interface{}) {	
+	var defaultValue *models.Item
+	if data == defaultValue {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
+
+func seedData() {
+	log.Println("Seeding data sample...")
+	items = models.DataSeed()
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
