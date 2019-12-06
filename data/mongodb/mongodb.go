52e4232916df71a35bc57fb63245dd346154a14d
Thiago Lunardi <thiago@thiagolunardi.net>
2019-12-06 20:51:25 +0100

    First async call with goroutines and channels

--- data/mongodb/mongodb.go
+++ data/mongodb/mongodb.go
@@ -1,17 +1,18 @@
 package mongodb
 
 import (
-	"strings"
 	"context"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/thiagolunardi/learning-golang/data/dberrors"
 	"github.com/thiagolunardi/learning-golang/models"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
@@ -28,17 +29,21 @@ var isInitialized bool
 func NewClient() (*Repo, error) {
 
 	if !isInitialized {
-	
+
 		dbClient, err := getClient()
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		ctx, _ := getContext()
 		dbClient.Connect(ctx)
 		defer dbClient.Disconnect(ctx)
 
 		err = dbClient.Ping(ctx, readpref.Primary())
-		if err != nil { log.Fatal(err)}
-		
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		log.Println("Using MongoDb database")
 
 		//dataSeed()
@@ -57,7 +62,7 @@ func getClient() (*mongo.Client, error) {
 	return mongo.NewClient(options.Client().ApplyURI("mongodb://0.0.0.0:27017"))
 }
 
-func getItemCollection(dbClient *mongo.Client) (*mongo.Collection) {
+func getItemCollection(dbClient *mongo.Client) *mongo.Collection {
 	return dbClient.Database("todo").Collection("items")
 }
 
@@ -67,15 +72,19 @@ func getContext() (context.Context, context.CancelFunc) {
 
 func getCollection(ctx context.Context) (*mongo.Collection, disconnectFunc) {
 	dbClient, err := getClient()
-	if err != nil { log.Fatal() }
+	if err != nil {
+		log.Fatal()
+	}
 
 	err = dbClient.Connect(ctx)
-	if err != nil { log.Fatal() }
+	if err != nil {
+		log.Fatal()
+	}
 
 	itemsCollection := getItemCollection(dbClient)
 
-	return itemsCollection, func() error { 
-		return dbClient.Disconnect(ctx) 
+	return itemsCollection, func() error {
+		return dbClient.Disconnect(ctx)
 	}
 }
 
@@ -84,20 +93,38 @@ func (repo *Repo) List(ctx context.Context) (models.Items, error) {
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	cur, err := itemsCollection.Find(ctx, bson.D{})
-	if err != nil { log.Fatal(err) }
-	defer cur.Close(ctx)
+	c := make(chan models.Item)
+	go listAsync(ctx, itemsCollection, c)
 
 	items := models.Items{}
-	for cur.Next(ctx) {
-		var item models.Item
-		err := cur.Decode(&item)
-		if err != nil { log.Fatal(err) }
-
+	for item := range c {
 		items = append(items, item)
 	}
 
 	return items, nil
+}
+
+func listAsync(
+	ctx context.Context,
+	itemsCollection *mongo.Collection,
+	c chan models.Item) {
+
+	cur, err := itemsCollection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var item models.Item
+		err := cur.Decode(&item)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c <- item
+	}
+	close(c)
 }
 
 // Get -
@@ -105,38 +132,70 @@ func (repo *Repo) Get(ctx context.Context, ID int) (*models.Item, error) {
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	filter := bson.M { "id": ID }
-	
+	c := make(chan *models.Item, 1)
+	go getAsync(ctx, itemsCollection, ID, c)
+	item := <-c
+
+	return item, nil
+}
+
+func getAsync(
+	ctx context.Context,
+	itemsCollection *mongo.Collection,
+	ID int,
+	c chan *models.Item) {
+	filter := bson.M{"id": ID}
+
 	item := models.Item{}
 	err := itemsCollection.FindOne(ctx, filter).Decode(&item)
-	
-	if err != nil { 
-		if strings.Contains(err.Error(), "no documents in result") {
-			return nil, nil
-		}
-		log.Fatal(err) 
-	}
 
-	return &item, err
+	if err == nil {
+		c <- &item
+	} else {
+		if strings.Contains(err.Error(), "no documents in result") {
+			c <- nil
+		}
+		close(c)
+		log.Fatal(err)
+	}
+	close(c)
 }
 
 // Create -
-func (repo *Repo) Create(ctx context.Context,item *models.Item) (*models.Item, error) {
+func (repo *Repo) Create(ctx context.Context, item *models.Item) (*models.Item, error) {
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	filter := bson.D {}
-	opts := options.FindOne().SetSort(bson.D{{"id", -1}})
+	c := make(chan *models.Item, 1)
+	go createAsync(ctx, itemsCollection, item, c)
+
+	createdItem := <-c
+
+	return createdItem, nil
+}
+
+func createAsync(
+	ctx context.Context,
+	itemsCollection *mongo.Collection,
+	item *models.Item,
+	c chan *models.Item) {
+	filter := bson.D{}
+	opts := options.FindOne().SetSort(bson.D{primitive.E{Key: "id", Value: -1}})
 	lastItem := models.Item{}
 	err := itemsCollection.FindOne(ctx, filter, opts).Decode(&lastItem)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	item.ID = lastItem.ID + 1
 
 	_, err = itemsCollection.InsertOne(ctx, item)
-	if err != nil { log.Fatal(err) }
-	
-	return item, nil
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c <- item
+	close(c)
 }
 
 // Update -
@@ -144,60 +203,106 @@ func (repo *Repo) Update(ctx context.Context, item *models.Item) (*models.Item, 
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	filter := bson.D {{ "id", item.ID }}
-	update := bson.D {{
-		"$set", 
-		bson.D {
-			{"done", item.Done },
-			{"title", item.Title},
-	}}}
+	c := make(chan *models.Item, 1)
+	go updateAsync(ctx, itemsCollection, item, c)
+
+	updatedItem := <-c
+	return updatedItem, nil
+}
+
+// Update -
+func updateAsync(
+	ctx context.Context,
+	itemsCollection *mongo.Collection,
+	item *models.Item,
+	c chan *models.Item) {
+
+	filter := bson.D{primitive.E{Key: "id", Value: item.ID}}
+	update := bson.D{primitive.E{
+		Key: "$set",
+		Value: bson.D{
+			primitive.E{Key: "done", Value: item.Done},
+			primitive.E{Key: "title", Value: item.Title},
+		}}}
 
 	result, err := itemsCollection.UpdateOne(ctx, filter, update)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		close(c)
+		log.Fatal(err)
+	}
 
 	if result.ModifiedCount == 0 {
+		close(c)
 		log.Fatal(dberrors.ErrItemNotFound)
 	}
 
-	return item, err
+	c <- item
+	close(c)
 }
 
 // Delete -
-func (repo *Repo) Delete(ctx context.Context,ID int) error {
+func (repo *Repo) Delete(ctx context.Context, ID int) error {
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	filter := bson.D {{ "id", ID }}
-	
-	_, err := itemsCollection.DeleteOne(ctx, filter)
-	if err != nil { log.Fatal(err) }
+	c := make(chan int64, 1)
+	go deleteAsync(ctx, itemsCollection, ID, c)
+	deleted := <- c
 
-	return err
+	log.Printf("%d deleted", deleted)
+
+	return nil
 }
+
+func deleteAsync(
+	ctx context.Context, 
+	itemsCollection *mongo.Collection,
+	ID int,
+	c chan int64) {
+
+	filter := bson.D{primitive.E{Key: "id", Value: ID}}
+
+	r, err := itemsCollection.DeleteOne(ctx, filter)
+	if err != nil {
+		close(c)
+		log.Fatal(err)
+	}
+	
+	c <- r.DeletedCount
+}
+
 
 func any(ctx context.Context) bool {
 	itemsCollection, disconnect := getCollection(ctx)
 	defer disconnect()
 
-	filter := bson.D {{ "_id", bson.D{{"$exists", true}}}}
+	filter := bson.D{primitive.E{Key: "_id", Value: bson.D{primitive.E{Key: "$exists", Value: true}}}}
 	cur, err := itemsCollection.Find(ctx, filter)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer cur.Close(ctx)
 
 	return cur.Next(ctx)
 }
 
 func dataSeed(ctx context.Context) {
-	
-	if any(ctx) { return }
+
+	if any(ctx) {
+		return
+	}
 
 	log.Println("Seeding data...")
 
 	dbClient, err := getClient()
-	if err != nil { log.Fatal() }
+	if err != nil {
+		log.Fatal()
+	}
 
 	err = dbClient.Connect(ctx)
-	if err != nil { log.Fatal() }
+	if err != nil {
+		log.Fatal()
+	}
 	defer dbClient.Disconnect(ctx)
 
 	itemsCollection := getItemCollection(dbClient)
